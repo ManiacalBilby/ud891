059021cd810fe9bd5c6811fd327b8469013080e2
ManiacalBilby <tylerwaynemcdonald@gmail.com>
2018-08-02 18:12:09 -0400

    Added role tags to radiogroup and each radio button. Added aria-checked atttribute to each radio button. Added setAttribute statements for aria-checked within changeFocus function.

--- lesson5-semantics-aria/06-radio-group/radiogroup.js
+++ lesson5-semantics-aria/06-radio-group/radiogroup.js
@@ -34,7 +34,7 @@
         button.tabIndex = "-1";
       }
 
-      // What about here?
+      
     }
 
   }
@@ -92,6 +92,7 @@
     // Set the old button to tabindex -1
     this.focusedButton.tabIndex = -1;
     this.focusedButton.removeAttribute('checked');
+    this.focusedButton.setAttribute('aria-checked', 'false')
 
     // Set the new button to tabindex 0 and focus it
     this.focusedButton = this.buttons[this.focusedIdx];
@@ -99,7 +100,7 @@
     this.focusedButton.focus();
     this.focusedButton.setAttribute('checked', '');
 
-    // ... we probably want to do some stuff here, too ...
+    this.focusedButton.setAttribute('aria-checked', 'true')
 
   };
 
